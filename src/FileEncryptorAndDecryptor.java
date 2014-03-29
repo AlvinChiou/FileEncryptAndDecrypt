60b452644cf262157548f7c04409f8d4188e7ce3
AlvinChiou <alvinhsian@gmail.com>
2014-03-30 03:26:00 +0800

    Reformate

--- src/FileEncryptorAndDecryptor.java
+++ src/FileEncryptorAndDecryptor.java
@@ -12,50 +12,50 @@ public class FileEncryptorAndDecryptor {
     private File file;
 
     public FileEncryptorAndDecryptor(String algo, String path) {
-        this.algo=algo; //setting algo
-        this.file=new File(path); //settong file
+        this.algo = algo; //setting algo
+        this.file = new File(path); //settong file
     }
 
-    public void encrypt() throws Exception{
+    public void encrypt() throws Exception {
         //opening streams
-        FileInputStream fis =new FileInputStream(file);
-        file=new File(file.getAbsolutePath()+".enc");
-        FileOutputStream fos =new FileOutputStream(file);
+        FileInputStream fis = new FileInputStream(file);
+        file = new File(file.getAbsolutePath() + ".enc");
+        FileOutputStream fos = new FileOutputStream(file);
         //generating key
         byte k[] = "HignDlPs".getBytes();
-        SecretKeySpec key = new SecretKeySpec(k,algo.split("/")[0]);
+        SecretKeySpec key = new SecretKeySpec(k, algo.split("/")[0]);
         //creating and initialising cipher and cipher streams
-        Cipher encrypt =  Cipher.getInstance(algo);
+        Cipher encrypt = Cipher.getInstance(algo);
         encrypt.init(Cipher.ENCRYPT_MODE, key);
-        CipherOutputStream cout=new CipherOutputStream(fos, encrypt);
+        CipherOutputStream cout = new CipherOutputStream(fos, encrypt);
 
         byte[] buf = new byte[1024];
         int read;
-        while((read=fis.read(buf))!=-1)  //reading data
-            cout.write(buf,0,read);  //writing encrypted data
+        while ((read = fis.read(buf)) != -1)  //reading data
+            cout.write(buf, 0, read);  //writing encrypted data
         //closing streams
         fis.close();
         cout.flush();
         cout.close();
     }
 
-    public void decrypt() throws Exception{
+    public void decrypt() throws Exception {
         //opening streams
-        FileInputStream fis =new FileInputStream(file);
-        file=new File(file.getAbsolutePath()+".dec");
-        FileOutputStream fos =new FileOutputStream(file);
+        FileInputStream fis = new FileInputStream(file);
+        file = new File(file.getAbsolutePath() + ".dec");
+        FileOutputStream fos = new FileOutputStream(file);
         //generating same key
         byte k[] = "HignDlPs".getBytes();
-        SecretKeySpec key = new SecretKeySpec(k,algo.split("/")[0]);
+        SecretKeySpec key = new SecretKeySpec(k, algo.split("/")[0]);
         //creating and initialising cipher and cipher streams
-        Cipher decrypt =  Cipher.getInstance(algo);
+        Cipher decrypt = Cipher.getInstance(algo);
         decrypt.init(Cipher.DECRYPT_MODE, key);
-        CipherInputStream cin=new CipherInputStream(fis, decrypt);
+        CipherInputStream cin = new CipherInputStream(fis, decrypt);
 
         byte[] buf = new byte[1024];
-        int read=0;
-        while((read=cin.read(buf))!=-1)  //reading encrypted data
-            fos.write(buf,0,read);  //writing decrypted data
+        int read = 0;
+        while ((read = cin.read(buf)) != -1)  //reading encrypted data
+            fos.write(buf, 0, read);  //writing decrypted data
         //closing streams
         cin.close();
         fos.flush();
